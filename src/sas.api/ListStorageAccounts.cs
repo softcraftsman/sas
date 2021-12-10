fc004769a4fb8d735658de8dbed4e2f876696627
John Brown <jobrown4@microsoft.com>
2021-12-10 16:42:16 -0500

    Added ListStorageAccounts function

---
+++ src/sas.api/ListStorageAccounts.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+using Azure.Storage.Files.DataLake;
+using Azure.Storage.Files.DataLake.Models;
+
+namespace sas.api.CreateTopLevelFolder
+{
+    public static class ListStorageAccounts
+    {
+        [FunctionName("ListStorageAccounts")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            var dlsa = Environment.GetEnvironmentVariable("DATALAKE_STORAGE_ACCOUNTS");
+            log.LogInformation($"Storage accounts from configuration: {dlsa}");
+            if (dlsa is null) {
+                dlsa = "Error: DATALAKE_STORAGE_ACCOUNTS is not set in application config. For local development use: local.settings.json";
+                return new BadRequestObjectResult(dlsa);
+            }
+
+            return new OkObjectResult(dlsa);
+        }
+    }
+}
