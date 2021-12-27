3760137808a99212a6f16e8c90d038e83f487f63
John Brown <jobrown4@microsoft.com>
2021-12-27 10:08:30 -0500

    Refactored to support getting the ObjectID from the UPN

--- src/sas.api/CreateTopLevelFolder.cs
+++ src/sas.api/CreateTopLevelFolder.cs
@@ -12,6 +12,7 @@ using Azure.Storage.Files.DataLake;
 using Azure.Storage.Files.DataLake.Models;
 using sas.api;
 using System.Text;
+using Microsoft.Identity.Client;
 
 namespace sas.api
 {
@@ -122,15 +123,111 @@ namespace sas.api
             log.LogInformation("Internal Server Error - 500. Only GET and POST are acceptable.");
             return new BadRequestResult();
         }
+
+        public static async Task<IActionResult> AlternateRun([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            // GET - Send Instructions back to calling client
+            if (req.Method == HttpMethods.Get) {
+                log.LogInformation("CreateTopLevelFolder GET method called.");
+                var tlfp = JsonConvert.SerializeObject(new TopLevelFolderParameters());
+                var help = $"Use a POST method with the body containing the properties of {nameof(TopLevelFolderParameters)}\n{tlfp}";
+                return new OkObjectResult(help);
+            }
+
+            // POST - Method
+            if (req.Method == HttpMethods.Post)
+            {
+                //Extracting body object from the call and deserializing it.
+                var tlfp = GetTopLevelFolderParameters(req, out string error);
+                if (error != null)
+                {
+                    log.LogInformation(error);
+                    return new BadRequestObjectResult(error);
+                }
+
+                // Get object ID from UPN
+                if (tlfp.FolderOwner.Contains('@'))
+                {
+                    var apiAccessToken = await GetApiToken(req, log, tlfp.FolderOwner);
+                    tlfp.FolderOwner = await UserOperations.GetObjectIdFromUPN(apiAccessToken, tlfp.FolderOwner);
+                }
+
+                // Call each of the steps in order and error out if anytyhing fails
+                var containerUri = new Uri($"https://{tlfp.StorageAcount}.dfs.core.windows.net/{tlfp.Container}");
+                var accessToken = UserOperations.GetAccessTokenFromRequest(req);
+
+                var adlOps = new ADLSOperations(containerUri, accessToken, log);
+                if (adlOps.AddsFolderOwnerToContainerACLAsExecute(tlfp.FolderOwner, tlfp.Container, true, tlfp.Container, out error)
+                    && adlOps.CreatesNewFolder(tlfp.Folder, tlfp.Container, out error)
+                    && adlOps.AssignsRWXToFolderOwner(tlfp.FolderOwner, tlfp.Container, tlfp.Folder, out error)
+                    && adlOps.SavesFundCodeIntoContainerMetadata(tlfp.FundCode, tlfp.Container, out error))
+                {
+                    log.LogInformation("success");
+                }
+                else
+                {
+                    log.LogError("Error: " + error);
+                    return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+
+                log.LogInformation("Workflow completed successfully.");
+                return new OkResult();
+            }
+
+            log.LogInformation("Internal Server Error - 500. Only GET and POST are acceptable.");
+            return new BadRequestResult();
+        }
+
+        private static TopLevelFolderParameters GetTopLevelFolderParameters(HttpRequest req, out string error)
+        {
+            string body = string.Empty;
+            error = null;
+
+            using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+            {
+                body = reader.ReadToEnd();
+            }
+
+            var bodyDeserialized = JsonConvert.DeserializeObject<TopLevelFolderParameters>(body);
+
+            //Performs call's body validation
+            if (bodyDeserialized is null)
+            {
+                error = $"{nameof(TopLevelFolderParameters)} is missing.";
+            }
+            else
+            {
+                if (bodyDeserialized.Container == null || bodyDeserialized.Folder == null ||
+                    bodyDeserialized.FolderOwner == null || bodyDeserialized.FundCode == null ||
+                    bodyDeserialized.StorageAcount == null)
+                {
+                    error = $"{nameof(TopLevelFolderParameters)} is malformed.";
+                }
+            }
+
+            return bodyDeserialized;
+        }
+
+        private static async Task<string> GetApiToken(HttpRequest req, ILogger log, string folderOwner)
+        {
+            var accessToken = UserOperations.GetAccessTokenFromRequest(req);
+            var userAssertion = new UserAssertion(accessToken);
+            var upn = UserOperations.GetObjectIdFromUPN(accessToken, folderOwner);
+            ConfidentialClientApplicationOptions options = new()
+            {
+                TenantId = Environment.GetEnvironmentVariable("TENANT_ID"),
+                ClientId = Environment.GetEnvironmentVariable("APP_REGISTRATION_CLIENT_ID"),
+                ClientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET"),
+            };
+            var app = ConfidentialClientApplicationBuilder.CreateWithApplicationOptions(options)
+                .Build();
+            var scopes = new string[] {
+                        "https://management.azure.com/user_impersonation",
+                        "https://storage.azure.com/user_impersonation"};
+
+            var authResult = await app.AcquireTokenOnBehalfOf(scopes, userAssertion)
+                .ExecuteAsync();
+            return authResult.AccessToken;
+        }
     }
-
-// {
-//     "StorageAcount": "test",
-//     "Container": "test",
-//     "Folder": "test",
-//     "FundCode": "test",
-//     "FolderOwner": "test"
-// }
-
-
 }
--- src/sas.api/Services/ADLSOperations.cs
+++ src/sas.api/Services/ADLSOperations.cs
@@ -4,9 +4,27 @@ using System.Threading.Tasks;
 using Azure.Storage;
 using Azure.Storage.Files.DataLake;
 using Azure.Storage.Files.DataLake.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Azure.Identity;
+using Microsoft.Identity.Client;
+using System.Net;
+using Microsoft.Graph;
 
 public class ADLSOperations
 {
+
+    private readonly ILogger log;
+    private readonly Uri containerUri;
+    private readonly string clientToken;
+    public ADLSOperations(Uri storageContainer, string token, ILogger log)
+    {
+        this.log = log;
+        this.containerUri = storageContainer;
+        this.clientToken = token;
+    }
+
+    #region Private Static Methods
     private static DataLakeServiceClient CreatesDataLakeConnection()
     {
         //Retrieving environment variables
@@ -36,7 +54,22 @@ public class ADLSOperations
 
         return fileSystem;
     }
+    #endregion
 
+    #region Private Methods
+
+    private DataLakeServiceClient CreateDlsClientFromToken()
+    {
+        var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+        var clientId = Environment.GetEnvironmentVariable("APP_REGISTRATION_CLIENT_ID");
+        var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+        var tokenCred = new OnBehalfOfCredential(tenantId, clientId, clientSecret, clientToken);
+        var dlsClient = new DataLakeServiceClient(containerUri, tokenCred);
+        return dlsClient;
+    }
+    #endregion
+
+    #region Public Static Methods
     public static bool AddsFolderOwnerToContainerACLAsExecute(string folderOwner, string container, bool isDefaultScope, string storageRootContainer)
     {
         var serviceClient = CreatesDataLakeConnection();
@@ -155,4 +188,89 @@ public class ADLSOperations
         directoryClient.SetAccessControlList(accessControlList);
 
     }
+    #endregion
+
+    #region Public Methods
+    public bool AddsFolderOwnerToContainerACLAsExecute(string folderOwner, string container, bool isDefaultScope, string storageRootContainer, out string error)
+    {
+        log.LogTrace($"Adding '{folderOwner}' (Fold Owner) to the container '{container}' as 'Execute'...");
+
+        var serviceClient = CreateDlsClientFromToken();
+        var directoryClient = GetsReferenceToContainer(serviceClient, storageRootContainer, "");
+
+        var accessControlListUpdate = new List<PathAccessControlItem>()
+        {
+            new PathAccessControlItem(AccessControlType.User, RolePermissions.Execute, isDefaultScope, entityId: folderOwner)
+        };
+
+        // Update root container's ACL
+        var response = directoryClient.UpdateAccessControlRecursive(accessControlListUpdate, null);
+        var statusFlag = response.GetRawResponse().Status == ((int)HttpStatusCode.OK);
+
+        if (statusFlag)
+            error = null;
+        else
+            error = "Error on trying to add Folder Owner as Execute on the root Container. Error 500.";
+        return statusFlag;
+    }
+    
+    public bool CreatesNewFolder(string folder, string storageRootContainer, out string error)
+    {
+        error = null;
+        log.LogTrace($"Creating the folder '{folder}' within the container '{storageRootContainer}'...");
+
+        var serviceClient = this.CreateDlsClientFromToken();
+        var directoryFileSystem = GetsReferenceToFileSystem(serviceClient, storageRootContainer);
+        DataLakeDirectoryClient directory = directoryFileSystem.CreateDirectory(folder);
+
+        var response = directory.Create();
+        var statusFlag = response.GetRawResponse().Status == 201;
+        if (!statusFlag)
+            error = "Error trying to create the new folder. Error 500.";
+        return statusFlag;
+    }
+    
+    public bool AssignsRWXToFolderOwner(string folderOwner, string storageRootContainer, string folder, out string error)
+    {
+        log.LogTrace($"Assigning RWX permission to Folder Owner ({folderOwner}) at folder's ({folder}) level...");
+
+        var storageClient = this.CreateDlsClientFromToken();
+        var directoryClient = GetsReferenceToContainer(storageClient, storageRootContainer, folder);
+        PathAccessControl directoryAccessControl = directoryClient.GetAccessControl();
+        var accessControlListUpdate = (List<PathAccessControlItem>)directoryAccessControl.AccessControlList;
+
+        int index = -1;
+        foreach (var item in accessControlListUpdate)
+        {
+            if (item.EntityId == folderOwner)
+            {
+                index = accessControlListUpdate.IndexOf(item);
+                break;
+            }
+        }
+
+        if (index > -1)
+        {
+            var acType = accessControlListUpdate[index].AccessControlType;
+            accessControlListUpdate[index] = new PathAccessControlItem(acType, RolePermissions.Read | RolePermissions.Write | RolePermissions.Execute, entityId: folderOwner);
+        }
+
+        var result = directoryClient.SetAccessControlList(accessControlListUpdate);
+        var statusFlag = result.GetRawResponse().Status == (int)HttpStatusCode.OK;
+
+        if (statusFlag)
+            error = null;
+        else
+            error = "Error trying to assign the RWX permission to the folder. Error 500.";
+        return statusFlag;
+    }
+
+    public bool SavesFundCodeIntoContainerMetadata(string fundCode, string container, out string error)
+    {
+        log.LogTrace($"Saving FundCode into container's metadata...");
+        var result = SavesFundCodeIntoContainerMetadata(fundCode, container);
+        error = result ? null : "Error trying to save de Fund Code into Container's metadata. Error 500.";
+        return result;
+    }
+    #endregion
 }
