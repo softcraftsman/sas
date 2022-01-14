f522740de208a2f2625cf1ebb31c38c83f327d00
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-01-13 20:40:41 -0600

    Change Roles Fx auth level to Anon

--- src/sas.api/Endpoints/Roles.cs
+++ src/sas.api/Endpoints/Roles.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace sas.api
@@ -11,13 +10,13 @@ namespace sas.api
 	public static class Roles
 	{
 		[FunctionName("RolesGET")]
-		public static async Task<string[]> RolesGET([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "Roles")]
+		public static async Task<string[]> RolesGET([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "Roles")]
 			HttpRequest req, ILogger log)
 		{
 			// Request body is supposed to contain the user's access token
-			string Body = await new StreamReader(req.Body).ReadToEndAsync();
+			//string Body = await new StreamReader(req.Body).ReadToEndAsync();
 
-			log.LogInformation($"Looking for custom roles to assign to '{Body}'.");
+			log.LogInformation($"Looking for custom roles to assign to '...'.");
 
 			return new List<string>() { "some-fake-role" }.ToArray();
 		}
