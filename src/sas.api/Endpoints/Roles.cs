20e1aefe1e6a68f690fd59b9841b3bc79fe13a52
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-01-13 21:06:44 -0600

    A whole bunch-a changes

--- src/sas.api/Endpoints/Roles.cs
+++ src/sas.api/Endpoints/Roles.cs
@@ -1,27 +1,29 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace sas.api
 {
 	public static class Roles
 	{
 		[FunctionName("RolesGET")]
-		public static RolesResult RolesGET([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "Roles")]
-			HttpRequest req, ILogger log)
+		public static IActionResult RolesGET([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "Roles")]
+			HttpRequest req)
 		{
 			// Request body is supposed to contain the user's access token
 			//string Body = await new StreamReader(req.Body).ReadToEndAsync();
 
-			log.LogInformation($"Looking for custom roles to assign to '...'.");
+			//log.LogInformation($"Looking for custom roles to assign to '...'.");
 
-			return new RolesResult()
+			RolesResult rr = new RolesResult()
 			{
-				Roles = new List<string>() { "some-fake-role" }.ToArray()
+				Roles = new string[] { "some-fake-role" }
 			};
+
+			return new OkObjectResult(rr);
 		}
 	}
 
