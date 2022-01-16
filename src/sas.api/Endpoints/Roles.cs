73cfc19fead0ae7ca90d90b4177a6e9fba5b8023
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-01-16 15:55:50 -0600

    Log input to api/Roles

--- src/sas.api/Endpoints/Roles.cs
+++ src/sas.api/Endpoints/Roles.cs
@@ -3,25 +3,32 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace sas.api
 {
 	public static class Roles
 	{
 		[FunctionName("Roles")]
-		public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "GET", "POST", Route = "Roles")]
+		public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "GET", "POST", Route = "Roles")]
 			HttpRequest req, ILogger log)
 		{
+			RolesResult rr = new RolesResult();
+
 			// Request body is supposed to contain the user's access token
-			//string Body = await new StreamReader(req.Body).ReadToEndAsync();
-
-			log.LogInformation($"Looking for custom roles to assign to '...'.");
-
-			RolesResult rr = new RolesResult()
+			if (req.Body.Length > 0)
 			{
-				Roles = new string[] { "some-fake-role", "second-fake-role" }
-			};
+				string Body = await new StreamReader(req.Body).ReadToEndAsync();
+
+				//log.LogInformation($"Looking for custom roles to assign to '...'.");
+				log.LogInformation($"Input\n{Body}");
+
+				rr = new RolesResult()
+				{
+					Roles = new string[] { "some-fake-role", "second-fake-role" }
+				};
+			}
 
 			return new OkObjectResult(rr);
 		}
