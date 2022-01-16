20b9e071a1c3f742bd4e54f3fafc6420061a9f85
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-01-16 17:13:06 -0600

    Update logged info

--- src/sas.api/Endpoints/Roles.cs
+++ src/sas.api/Endpoints/Roles.cs
@@ -27,12 +27,14 @@ namespace sas.api
 				log.LogInformation($"Looking for custom roles to assign to '{it.UserDetails}'.");
 
 				string[] additionalRoles = it.Claims
+					// Find any roles claims in the token
 					.Where(c => c.Type.Equals("http://schemas.microsoft.com/ws/2008/06/identity/claims/role")
-								|| c.Type.Equals("roles"))
+							 || c.Type.Equals("roles"))
+					// Get those values as an array of strings
 					.Select(c => c.Value)
 					.ToArray();
 
-				log.LogInformation($"Assigning additional roles '{additionalRoles.SelectMany(r => r)}' to '{it.UserDetails}'.");
+				log.LogInformation($"Assigning {additionalRoles.Length} additional role(s) '{string.Join(',', additionalRoles)}' to '{it.UserDetails}'.");
 
 				rr.Roles = additionalRoles;
 			}
