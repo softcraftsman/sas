affebd75b008f09e57e744802c70d2887ef3b1cc
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-02-05 21:50:20 -0600

    Initial review Comments and minor optimizations

--- src/sas.front/src/components/StorageAccountsPage/StorageAccountsPage.js
+++ src/sas.front/src/components/StorageAccountsPage/StorageAccountsPage.js
@@ -14,143 +14,144 @@ import Selector from '../Selector'
  * Renders list of Storage Accounts
  */
 const StorageAccountsPage = ({ strings }) => {
-    const { account, isAuthenticated } = useAuthentication()
+	const { account, isAuthenticated } = useAuthentication()
 
-    const [selectedStorageAccount, setSelectedStorageAccount] = useState('')
-    const [selectedFileSystem, setSelectedFileSystem] = useState('')
+	const [selectedStorageAccount, setSelectedStorageAccount] = useState('')
+	const [selectedFileSystem, setSelectedFileSystem] = useState('')
 
-    const [storageAccounts, setStorageAccounts] = useState([])
-    const [directories, setDirectories] = useState([])
+	const [storageAccounts, setStorageAccounts] = useState([])
+	const [directories, setDirectories] = useState([])
 
-    const [toastMessage, setToastMessage] = useState()
-    const [isToastOpen, setToastOpen] = useState(false)
+	const [toastMessage, setToastMessage] = useState()
+	const [isToastOpen, setToastOpen] = useState(false)
 
-    // Retrieve the list of File Systems for the selected Azure Data Lake Storage Account
-    useEffect(() => {
-        const retrieveStorageAccounts = async () => {
-            try {
-                const _storageAccounts = await getFileSystems()
-                setStorageAccounts(_storageAccounts)
-            }
-            catch (error) {
-                // console.log(error)
-            }
-        }
+	// Retrieve the list of File Systems for the selected Azure Data Lake Storage Account
+	useEffect(() => {
+		const retrieveStorageAccounts = async () => {
+			try {
+				const _storageAccounts = await getFileSystems()
+				setStorageAccounts(_storageAccounts)
+			}
+			catch (error) {
+				console.error(error);
+			}
+		}
 
-        isAuthenticated && retrieveStorageAccounts()
-    }, [isAuthenticated])
-
-
-    // Retrieve the list of Directories for the selected File System
-    useEffect(() => {
-        const retrieveDirectories = async (storageAccount, fileSystem) => {
-            //const toSpace = kb => `${kb} KB`
-
-            try {
-                const _directories = await getDirectories(storageAccount, fileSystem)
-                setDirectories(_directories)
-            }
-            catch (error) {
-                // console.log(error)
-            }
-        }
-
-        selectedFileSystem && retrieveDirectories(selectedStorageAccount, selectedFileSystem)
-    }, [selectedStorageAccount, selectedFileSystem])
+		isAuthenticated && retrieveStorageAccounts()
+	}, [isAuthenticated])
 
 
-    const displayToast = message => {
-        setToastMessage(message)
-        setToastOpen(true)
-    }
+	// Retrieve the list of Directories for the selected File System
+	useEffect(() => {
+		const retrieveDirectories = async (storageAccount, fileSystem) => {
+			//const toSpace = kb => `${kb} KB`
+
+			try {
+				const _directories = await getDirectories(storageAccount, fileSystem)
+				setDirectories(_directories)
+			}
+			catch (error) {
+				console.error(error);
+			}
+		}
+
+		selectedFileSystem && retrieveDirectories(selectedStorageAccount, selectedFileSystem)
+	}, [selectedStorageAccount, selectedFileSystem])
 
 
-    const handleCreateDirectory = (data) => {
-        // Calls the API to save the directory
-        createFolder(selectedStorageAccount, selectedFileSystem, account.userDetails, data)
-            .then((newDirectory) => {
-                const _directories = [...directories, newDirectory].sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
-                setDirectories(_directories)
-
-                // Display a toast
-                displayToast(strings.directoryCreated(newDirectory.name))
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
+	const displayToast = message => {
+		setToastMessage(message)
+		setToastOpen(true)
+	}
 
 
-    const handleStorageAccountChange = useCallback(id => {
-        setSelectedStorageAccount(id)
-    }, [])
+	const handleCreateDirectory = (data) => {
+		// Calls the API to save the directory
+		createFolder(selectedStorageAccount, selectedFileSystem, account.userDetails, data)
+			.then((newDirectory) => {
+				// TODO: Match sort order to sort order from API (by URI)
+				const _directories = [...directories, newDirectory].sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+				setDirectories(_directories)
+
+				// Display a toast
+				displayToast(strings.directoryCreated(newDirectory.name))
+			})
+			.catch(error => {
+				console.error(error)
+			})
+	}
 
 
-    const handleFileSystemChange = useCallback(id => {
-        setSelectedFileSystem(id)
-    }, [])
+	const handleStorageAccountChange = useCallback(id => {
+		setSelectedStorageAccount(id)
+	}, [])
 
 
-    const storageAccountItems = storageAccounts.map(account => account.name)
-    const fileSystemItems = selectedStorageAccount ? storageAccounts.find(account => account.name === selectedStorageAccount).fileSystems : []
+	const handleFileSystemChange = useCallback(id => {
+		setSelectedFileSystem(id)
+	}, [])
 
 
-    return (
-        <>
-            <Container>
-                <Grid container spacing={2} sx={{ justifyContent: 'center', marginBottom: '10px' }}>
-                    <Grid item md={6}>
-                        <Selector
-                            id='storageAccountSelector'
-                            items={storageAccountItems}
-                            label={strings.storageAccountLabel}
-                            onChange={handleStorageAccountChange}
-                            selectedItem={selectedStorageAccount}
-                        />
-                    </Grid>
-                    <Grid item md={6}>
-                        <Selector
-                            id='fileSystemSelector'
-                            items={fileSystemItems}
-                            label={strings.fileSystemLabel}
-                            onChange={handleFileSystemChange}
-                            selectedItem={selectedFileSystem}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <DirectoriesManager
-                            data={directories}
-                            onCreateDirectory={handleCreateDirectory}
-                            strings={strings}
-                        />
-                    </Grid>
-                </Grid>
-            </Container>
+	const storageAccountItems = storageAccounts.map(account => account.name)
+	const fileSystemItems = selectedStorageAccount ? storageAccounts.find(account => account.name === selectedStorageAccount).fileSystems : []
 
-            <Snackbar
-                open={isToastOpen}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-                autoHideDuration={5000}
-                onClose={() => setToastOpen(false)}
-            >
-                <Alert severity='success'>{toastMessage}</Alert>
-            </Snackbar>
-        </>
-    )
+
+	return (
+		<>
+			<Container>
+				<Grid container spacing={2} sx={{ justifyContent: 'center', marginBottom: '10px' }}>
+					<Grid item md={6}>
+						<Selector
+							id='storageAccountSelector'
+							items={storageAccountItems}
+							label={strings.storageAccountLabel}
+							onChange={handleStorageAccountChange}
+							selectedItem={selectedStorageAccount}
+						/>
+					</Grid>
+					<Grid item md={6}>
+						<Selector
+							id='fileSystemSelector'
+							items={fileSystemItems}
+							label={strings.fileSystemLabel}
+							onChange={handleFileSystemChange}
+							selectedItem={selectedFileSystem}
+						/>
+					</Grid>
+					<Grid item>
+						<DirectoriesManager
+							data={directories}
+							onCreateDirectory={handleCreateDirectory}
+							strings={strings}
+						/>
+					</Grid>
+				</Grid>
+			</Container>
+
+			<Snackbar
+				open={isToastOpen}
+				anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+				autoHideDuration={5000}
+				onClose={() => setToastOpen(false)}
+			>
+				<Alert severity='success'>{toastMessage}</Alert>
+			</Snackbar>
+		</>
+	)
 }
 
 StorageAccountsPage.propTypes = {
-    strings: PropTypes.shape({
-        fileSystemLabel: PropTypes.string,
-        storageAccountLabel: PropTypes.string
-    })
+	strings: PropTypes.shape({
+		fileSystemLabel: PropTypes.string,
+		storageAccountLabel: PropTypes.string
+	})
 }
 
 StorageAccountsPage.defaultProps = {
-    strings: {
-        fileSystemLabel: 'File System',
-        storageAccountLabel: 'Storage Account'
-    }
+	strings: {
+		fileSystemLabel: 'File System',
+		storageAccountLabel: 'Storage Account'
+	}
 }
 
 export default StorageAccountsPage
